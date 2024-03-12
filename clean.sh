926f2b2c578fef8c0a6856b7f1279e8de2064700
me4tw <me4tw.b@gmail.com>
2024-03-12 17:45:49 +1000

    tileset

---
+++ clean.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+rm -rf ./scripts/__pycache__
+rm -rf ./engine/tilemap/management/__pycache__
+rm -rf ./engine/tilemap/__pycache__
+rm -rf ./engine/other/__pycache__
+rm -rf ./engine/map/__pycache__
+rm -rf ./engine/image/__pycache__
+rm -rf ./engine/entities/__pycache__
+rm -rf ./engine/core/__pycache__
+rm -rf ./engine/context/__pycache__
+rm -rf ./engine/__pycache__
+
